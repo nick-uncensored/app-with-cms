3db9421a96b1b91cb29996e7348d2f9ed9769af6
Nicholas Kaufmann <nick@uncensored.tech>
2019-10-14 03:17:59 -0400

    cms update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import './globalStyles.css'
 import App from './App'
 import registerServiceWorker, { unregister } from './registerServiceWorker'
 import data from './data.json'
-
+/*
+// This form is setup to use Netlify's form handling:the form action is set to the current absolute url: action: '/contact/'a name attribute is sent with the form's data 'form-name': 'Contact'netlify data attributes are added to the form data-netlify data-netlify-honeypotFind out more in the Netlify Docs.
+*/
 const rootEl = document.getElementById('root')
 render(<App />, rootEl)
 
